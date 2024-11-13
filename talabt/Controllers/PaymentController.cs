820ed503361822c6c988e98c722116ed0f595662
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-11-13 18:53:18 +0200

    Create Or Update PaymentIntent Endpoint

---
+++ talabt/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using talabat.Core.ServicesContext;
+using talabt.Controllers;
+using talabt.Error;
+using talabtAPIs.DTOs;
+
+namespace talabtAPIs.Controllers
+{
+    
+    public class PaymentController : BaseApiController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasketDTO>> CreateOrUpdatePaymentIntent(string basketId) 
+        {
+        var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+        if (basket is null) return BadRequest(new ApiErrorResponse( 400,"There is a problem with your basket"));
+        return Ok(basket);
+        
+        }
+    }
+}
